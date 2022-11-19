f91519ee6cefa2ac2d5c674ef59c94722634bb9b
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2022-11-19 13:36:53 -0500

    updated-module-description

--- mod.ts
+++ mod.ts
@@ -1,20 +1,20 @@
 // Copyright 2022 mConnect.biz. All rights reserved. MIT license.
 
 /**
- * A test module testing Deno modules and applications.
+ * Simple and Hash In-Memory Information Cache.
  *
  * ### Example Usage:
  *
  * Test case composition - see the test folder
  *
  * ```ts
- * import { assertEquals, assertNotEquals, postTestResult } from "https://deno.land/x/mctest/mod.ts";
+ * import { assertEquals, assertNotEquals, postTestResult } from "https://deno.land/x/mcache/mod.ts";
  *
  * ```
  *
  * To test the module run:
  * ```sh
- * deno run test/testCases.ts
+ * deno run test.ts
  * ```
  *
  * @module
